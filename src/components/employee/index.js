2eeceeb42293113c9d57a3930bf289c0e6c02eba
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-02-17 12:09:06 -0800

    got result from API request to show one employee on table; gonna create loop to add all employees into table

--- src/components/employee/index.js
+++ src/components/employee/index.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { Component, useState, useEffect } from "react";
+import API from "../utils/API";
 // import API from "../utils/API"
 
 // need to get picture in too
 
 function EmployeeCard() {
+
+    const [empList, setList] = useState([]);
+    // const [error, setError] = useState("");
+
+
+    // 
+    useEffect(() => {
+        API.search()
+        .then((res) => {
+            console.log(res);
+            setList(res.data.results);
+        })
+          
+    }, []);
+
     return (
         <table style={{ "width": "100%" }}>
             <tr>
@@ -13,17 +29,18 @@ function EmployeeCard() {
                 <th>Phone</th>
                 <th>Cell</th>
             </tr>
+
             <tr>
                 <td>
                     <img
                         alt="placeholder"
-                        src="https://via.placeholder.com/150"
+                        src={empList[0].picture.thumbnail}
                     />
                 </td>
-                <td>John Smith</td>
-                <td>test@test.com</td>
-                <td>1-234-5678</td>
-                <td>1-876-5432</td>
+                <td>{empList[0].name.first}</td>
+                <td>{empList[0].email}</td>
+                <td>{empList[0].phone}</td>
+                <td>{empList[0].cell}</td>
             </tr>
         </table>
     )
